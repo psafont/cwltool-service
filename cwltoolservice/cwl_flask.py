07d6648d812f95f05f79fd106dd15601f5d81ed4
Pau Ruiz i Safont <psafont@ebi.ac.uk>
2017-06-13 15:12:54 +0100

    maintenance: make pylint happier

--- cwltoolservice/cwl_flask.py
+++ cwltoolservice/cwl_flask.py
@@ -1,26 +1,27 @@
-from os import path
+import os
 from subprocess import Popen, PIPE
 from tempfile import mkstemp, mkdtemp
 from json import dumps, loads
-from yaml import load
 from signal import SIGQUIT, SIGTSTP, SIGCONT
 from threading import Lock, Thread
 from time import sleep
 from copy import copy
+from yaml import load
 
 from future.utils import iteritems
 
 from flask import Flask, Response, request, redirect, abort, send_from_directory
 from flask_cors import CORS
 
-app = Flask(__name__)
-CORS(app)
+APP = Flask(__name__)
+CORS(APP)
 
-jobs_lock = Lock()
-jobs = []
+JOBS_LOCK = Lock()
+JOBS = []
 
 
 class Job(Thread):
+    # pylint: disable=too-many-instance-attributes
     def __init__(self, jobid, path, inputobj):
         super(Job, self).__init__()
         self.jobid = jobid
@@ -82,18 +83,18 @@ class Job(Thread):
                 self.status["state"] = "Running"
 
 
-@app.route("/run", methods=['POST'])
+@APP.route("/run", methods=['POST'])
 def runworkflow():
     path = request.args["wf"]
-    with jobs_lock:
-        jobid = len(jobs)
+    with JOBS_LOCK:
+        jobid = len(JOBS)
         job = Job(jobid, path, request.stream.read())
         job.start()
-        jobs.append(job)
+        JOBS.append(job)
     return redirect("/jobs/%i" % jobid, code=303)
 
 
-@app.route("/jobs/<int:jobid>", methods=['GET', 'POST'])
+@APP.route("/jobs/<int:jobid>", methods=['GET', 'POST'])
 def jobcontrol(jobid):
     job = getjob(jobid)
     if not job:
@@ -120,7 +121,7 @@ def jobcontrol(jobid):
     return dumps(status, indent=4), 200, ""
 
 
-@app.route("/jobs/<int:jobid>/log", methods=['GET'])
+@APP.route("/jobs/<int:jobid>/log", methods=['GET'])
 def getlog(jobid):
     job = getjob(jobid)
     if not job:
@@ -129,7 +130,7 @@ def getlog(jobid):
     return Response(logspooler(job))
 
 
-@app.route("/jobs/<int:jobid>/output/<string:outputid>", methods=['GET'])
+@APP.route("/jobs/<int:jobid>/output/<string:outputid>", methods=['GET'])
 def getoutput(jobid, outputid):
     job = getjob(jobid)
     if not job:
@@ -146,11 +147,11 @@ def getoutput(jobid, outputid):
     return send_from_directory(path, filename)
 
 
-@app.route("/jobs", methods=['GET'])
+@APP.route("/jobs", methods=['GET'])
 def getjobs():
-    with jobs_lock:
-        jobscopy = copy(jobs)
-    return Response(spool(jobscopy))
+    with JOBS_LOCK:
+        jobs = copy(JOBS)
+    return Response(spool(jobs))
 
 
 def getoutputobj(job, outputid):
@@ -159,14 +160,14 @@ def getoutputobj(job, outputid):
 
 
 def getfile(output):
-    return path.split(output["path"])
+    return os.path.split(output["path"])
 
 
 def getjob(jobid):
     job = ""
-    with jobs_lock:
-        if 0 <= jobid < len(jobs):
-            job = jobs[jobid]
+    with JOBS_LOCK:
+        if 0 <= jobid < len(JOBS):
+            job = JOBS[jobid]
     return job
 
 
@@ -181,11 +182,11 @@ def spool(jobs):
 
 
 def logspooler(job):
-    with open(job.logname, "r") as f:
+    with open(job.logname, "r") as logfile:
         while True:
-            r = f.read(4096)
-            if r:
-                yield r
+            buf = logfile.read(4096)
+            if buf:
+                yield buf
             else:
                 with job.updatelock:
                     if job.status["state"] != "Running":
@@ -195,4 +196,4 @@ def logspooler(job):
 
 if __name__ == "__main__":
     # app.debug = True
-    app.run('0.0.0.0')
+    APP.run('0.0.0.0')
