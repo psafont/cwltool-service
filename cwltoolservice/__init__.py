849e0e5d32c8d574715d88776a70e3194ccb158d
Pau Ruiz i Safont <psafont@ebi.ac.uk>
2017-11-02 11:52:15 +0000

    maintenance: encapsulate server configuration

--- cwltoolservice/__init__.py
+++ cwltoolservice/__init__.py
@@ -7,11 +7,6 @@ from aap_client.crypto_files import load_public_from_x509
 from aap_client.crypto_files import load_private_from_pem
 from aap_client.flask.client import JWTClient
 
-APP = Flask(__name__, instance_relative_config=True)
-
-CORS(APP)
-JWT = JWTClient(APP)
-
 JOBS_LOCK = Lock()
 JOBS = []
 
@@ -20,16 +15,27 @@ USER_OWNS = dict()
 # store which jobs are owned by a user (user: [job])
 JOBS_OWNED_BY = dict()
 
-# configure
-APP.config[u'JWT_IDENTITY_CLAIM'] = u'sub'
-APP.config[u'JWT_ALGORITHM'] = u'RS256'
 
-APP.config.from_pyfile('application.cfg')
+def app():
+    app = Flask(__name__, instance_relative_config=True)
 
-private_key_secret = APP.config[u'PRIVATE_KEY_PASSCODE']
-key = load_private_from_pem(APP.config[u'PRIVATE_KEY_FILE'],
-                            secret=private_key_secret)
-APP.config[u'JWT_SECRET_KEY'] = key
+    CORS(app)
+    jwt = JWTClient(app)
 
-public_key = load_public_from_x509(APP.config[u'X509_FILE'])
-APP.config[u'JWT_PUBLIC_KEY'] = public_key
+    # configure
+    app.config[u'JWT_IDENTITY_CLAIM'] = u'sub'
+    app.config[u'JWT_ALGORITHM'] = u'RS256'
+
+    app.config.from_pyfile('application.cfg')
+
+    private_key_secret = app.config[u'PRIVATE_KEY_PASSCODE']
+    key = load_private_from_pem(app.config[u'PRIVATE_KEY_FILE'],
+                                secret=private_key_secret)
+    app.config[u'JWT_SECRET_KEY'] = key
+
+    public_key = load_public_from_x509(app.config[u'X509_FILE'])
+    app.config[u'JWT_PUBLIC_KEY'] = public_key
+    return app, jwt
+
+
+APP, JWT = app()
