a7f7526d2061f431829e9f8d8a894be3c472b295
Pau Ruiz i Safont <psafont@ebi.ac.uk>
2018-04-18 17:07:00 +0100

    maintenance: update dependencies, reorganize init

--- workflow_service/__init__.py
+++ workflow_service/__init__.py
@@ -11,18 +11,10 @@ from aap_client.crypto_files import (
 from workflow_service.database import init_db
 # import all db models before initialising the database
 # so they are registered properly on the metadata.
-import workflow_service.models  # pylint: disable=unused-import
+from workflow_service import models  # pylint: disable=unused-import
 
-def app():
-    web_app = Flask(__name__, instance_relative_config=True)
 
-    CORS(web_app)
-    JWTClient(web_app)
-
-    # set up new url mapper to load uuids
-    from workflow_service.decorators import UUIDConverter
-    web_app.url_map.converters['uuid'] = UUIDConverter
-
+def init_loggers(web_app):
     # flask is naughty and sets up default handlers
     # some spanking is in order
     del web_app.logger.handlers[:]
@@ -35,14 +27,15 @@ def app():
     web_app.logger.addHandler(handler)
     web_app.logger.setLevel(logging.ERROR)
 
-    # load database
-    init_db()
+def init_extensions(web_app):
+    CORS(web_app)
+    JWTClient(web_app)
 
-    # configure
+def configure_app(web_app, config):
     web_app.config[u'JWT_IDENTITY_CLAIM'] = u'sub'
     web_app.config[u'JWT_ALGORITHM'] = u'RS256'
 
-    web_app.config.from_pyfile('application.cfg')
+    web_app.config.from_pyfile(config)
 
     private_key_secret = web_app.config[u'PRIVATE_KEY_PASSCODE']
     key = load_private_from_pem(web_app.config[u'PRIVATE_KEY_FILE'],
@@ -51,4 +44,19 @@ def app():
 
     public_key = load_public_from_x509(web_app.config[u'X509_FILE'])
     web_app.config[u'JWT_PUBLIC_KEY'] = public_key
+
+def app(config='application.cfg'):
+    web_app = Flask(__name__, instance_relative_config=True)
+
+    init_db()
+
+    # set up new url mapper to load uuids
+    from workflow_service.decorators import UUIDConverter
+    web_app.url_map.converters['uuid'] = UUIDConverter
+
+    init_loggers(web_app)
+    configure_app(web_app, config)
+
+    init_extensions(web_app)
+
     return web_app
